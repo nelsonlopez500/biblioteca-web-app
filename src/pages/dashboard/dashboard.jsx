169d94fc9b1a1e4c416a8b7d2b76b3dde55df02a
Christian6114 <christianpleitez00@gmail.com>
2024-11-18 14:28:28 -0600

    actualizacion dashboard

--- src/pages/dashboard/dashboard.jsx
+++ src/pages/dashboard/dashboard.jsx
@@ -17,41 +17,41 @@ const Dashboard = () => {
             <Sidebar />
             <div className="title-bar-container-dashboard">
                 <div className="title-bar-dashboard">
-                    <h2>Sistema de Gestion de BiblioTK</h2>
+                    <h2>Sistema de Gestion de Penales</h2>
                 </div>
             </div>
             <div className="cards-container">
                 <div className="card" onClick={() => handleCardClick('/libros')}>
                     <img src="https://climatica.coop/wp-content/uploads/2024/01/1280px-Books_HD_8314929977-900x600.jpg?w=390&q=90" className="card-img-top" alt="Libros" />
                     <div className="card-body">
-                        <h3>Libros</h3>
-                        <p>Ir a la sección de Libros</p>
+                        <h3>Reclusos</h3>
+                        <p>Ir a la sección de Reclusos</p>
                     </div>
                 </div>
                 <div className="card" onClick={() => handleCardClick('/prestamos')}>
                     <img src="https://www.comunidadbaratz.com/wp-content/uploads/Hay-muchisimos-libros-en-las-bibliotecas-pero-solamente-unos-pocos-comparten-el-privilegio-de-ser-los-mas-prestados-1.jpg" className="card-img-top" alt="Préstamos" />
                     <div className="card-body">
-                        <h3>Préstamos</h3>
-                        <p>Ir a la sección de Préstamos</p>
+                        <h3>Penales</h3>
+                        <p>Ir a la sección de Penales</p>
                     </div>
                 </div>
                 <div className="card" onClick={() => handleCardClick('/editoriales')}>
                     <img src="https://p4.wallpaperbetter.com/wallpaper/944/111/648/book-books-culture-edition-wallpaper-preview.jpg" className="card-img-top" alt="Editoriales" />
                     <div className="card-body">
-                        <h3>Editoriales</h3>
-                        <p>Ir a la sección de Editoriales</p>
+                        <h3>Empleados</h3>
+                        <p>Ir a la sección de Empleados</p>
                     </div>
                 </div>
                 <div className="card" onClick={() => handleCardClick('/usuarios')}>
                     <img src="https://www.creaxid.com.mx/blog/wp-content/uploads/2021/08/investigacion-usuarios.jpg" className="card-img-top" alt="Usuarios" />
                     <div className="card-body">
-                        <h3>Usuarios</h3>
-                        <p>Ir a la sección de Usuarios</p>
+                        <h3>Planillas</h3>
+                        <p>Ir a la sección de Planillas</p>
                     </div>
                 </div>
             </div>
             <footer className="dashboard-footer">
-                © 2024 Sistema de gestión de biblioteca. Todos los derechos reservados.
+                © 2024 Sistema de gestión de penales. Todos los derechos reservados.
             </footer>
         </div >
     );
